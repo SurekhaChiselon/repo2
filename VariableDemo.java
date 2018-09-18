bb6cb48334f439624450ca011eb7edad07f54d14
SurekhaChiselon <surekha.satya@gmail.com>
2018-09-18 12:47:53 +0530

    This is 2nd time in day2

---
+++ VariableDemo.java
@@ -0,0 +1,31 @@
+package javapractice1;
+
+public class VariableDemo {
+
+	//program on data types
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean b=true;
+		char c=70;
+		char a='M';
+		byte B=10;
+		short s=300;
+		int i=3200;
+		long l=120000;
+		float f1=(float)23.01;
+		float f2=43.2f;
+		double d1=54.91;
+		double d3= 175.85245d;
+		int i1= (int)45.123;
+		System.out.println("data types are  "   +b);
+		System.out.println("data types are  " +c +a);
+		System.out.println("data types "  +b+c +a +B+s +i +l +f1 +f2  +d1 +d3 +i1);
+		//System.out.println("data types are"+b,+c,+a,+B,+s,+i,+l,+f1,+f2,+d1,+d3,i1);
+		//System.out.println("data types are"+b,+c,+a,+B,+s,+i,+l,+f1,+f2,+d1,+d3,i1);
+		//System.out.println("data types are"+b,+c,+a,+B,+s,+i,+l,+f1,+f2,+d1,+d3,i1);
+		
+	}
+
+}
